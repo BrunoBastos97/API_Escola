702732272a6f087be1daad02b83d5562f3033995
Bruno Bastos <brunomsb64@gmail.com>
2021-01-28 11:04:35 -0300

    main - criando o form cadastrargabarito

---
+++ public_html/formCadastraGabarito.php
@@ -0,0 +1,10 @@
+
+
+<form action="api/gabarito" method="post">
+    <h1> Cadastrar Gabarito </h1>
+    <br>
+    Pergunta: <input type="text" name="pergunta" id="pergunta">
+    Resposta Correta: <input type="text" name="idRespostaCorreta" id="idRespostaCorreta">
+    <br>
+    <input type="submit" value="enviar">
+</form>
