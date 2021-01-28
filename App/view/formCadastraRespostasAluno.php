b5fc43797ebca1df3b6dfcfbbb27ec28355b34ce
Bruno Bastos <brunomsb64@gmail.com>
2021-01-28 14:05:33 -0300

    main - forms

---
+++ App/view/formCadastraRespostasAluno.php
@@ -0,0 +1,10 @@
+
+<form action="../../public_html/api/alunoResposta" method="post">
+    <h1> Cadastrar Respostas do Aluno </h1>
+    <br>
+    ID Aluno: <input type="text" name="id_aluno" id="id_aluno">
+    ID Gabarito: <input type="text" name="id_gabarito" id="id_gabarito">
+    ID Resposta: <input type="text" name="idResposta" id="idResposta">
+    <br>
+    <input type="submit" value="enviar">
+</form>
