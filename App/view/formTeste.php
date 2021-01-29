90e6ff6845596dcb308bf796f8ff283f67aeff2f
Bruno Bastos <brunomsb64@gmail.com>
2021-01-28 21:11:00 -0300

    main - correção

--- App/view/formTeste.php
+++ App/view/formTeste.php
@@ -1,28 +1,28 @@
 <form action="../../public_html/api/gabarito" method="post">
     ID da Pergunta 
-    <input type="text" id="id" name="id">
+    <input type="text" id="id" name="id" value="10">
     <br>
     Pergunta
-    <input type="text" id="pergunta" name="pergunta">
+    <input type="text" id="pergunta" name="pergunta" value="sim">
     <br>
     Resposta Correta
-    <input type="text" id="idRespostaCorreta" name="idRespostaCorreta">
+    <input type="text" id="idRespostaCorreta" name="idRespostaCorreta" value="a">
     <br>
     <input type="submit" value="Teste cadastrar o Gabarito da prova">
 </form>
 
 <form action="../../public_html/api/alunoResposta" method="post">
     ID da Pergunta
-    <input type="text" name="id" id="id">
+    <input type="text" name="id" id="id" value="20">
     <br>
     ID Aluno
-    <input type="text" name="id_aluno" id="id_aluno">
+    <input type="text" name="id_aluno" id="id_aluno" value="2">
     <br>
     ID Gabarito
-    <input type="text" name="id_gabarito" id="id_gabarito">
+    <input type="text" name="id_gabarito" id="id_gabarito" value="10">
     <br>
     ID Resposta
-    <input type="text" name="idResposta" id="idResposta">
+    <input type="text" name="idResposta" id="idResposta" value="a">
     <br>
     <input type="submit" value="Teste cadastrar as resposta de cada aluno para cada prova">
 </form>
